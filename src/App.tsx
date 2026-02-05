b851101af335e8f460a64416498832266d66944e
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-02-05 22:44:18 +0100

    feat: add data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,49 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/card/Card";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 
+type MovieProps = {
+  id: number;
+  image: {
+    medium: string;
+  };
+  name: string;
+  premiered: string;
+  rating: {
+    average: number | null;
+  };
+};
+
 function App() {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch("https://api.tvmaze.com/shows?page=1");
+
+      const data = await response.json();
+      setMovies(data);
+    };
+
+    fetchMovies();
+  }, []);
+
   return (
     <>
       <Header></Header>
-      <Card
-        img="https://static.tvmaze.com/uploads/images/original_untouched/1/4600.jpg"
-        title="Kirbsy bucket"
-        date="21-08-2025"
-        rating={7}
-      ></Card>
+      <div className="card__container">
+        {movies.map((movie) => (
+          <Card
+            key={movie.id}
+            img={movie.image.medium}
+            title={movie.name}
+            date={movie.premiered}
+            rating={movie.rating.average}
+          />
+        ))}{" "}
+      </div>
       <Footer />
     </>
   );
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -4,23 +4,25 @@ type CardProps = {
   img: string;
   title: string;
   date: string;
-  rating: number;
+  rating: number | null;
 };
 
 export default function Card({ img, title, date, rating }: CardProps) {
   return (
     <>
-      <div className="card">
-        <div className="card__poster">
-          <img className="card__image" src={img} alt={title} />
-        </div>
+      <div>
+        <div className="card">
+          <div className="card__poster">
+            <img className="card__image" src={img} alt={title} />
+          </div>
 
-        <div className="card__rating">
-          <span className="card__rating-value">{rating}%</span>
-        </div>
+          <div className="card__rating">
+            <span className="card__rating-value">{rating}%</span>
+          </div>
 
-        <h3 className="card__title">{title.toUpperCase()}</h3>
-        <div className="card__date">{date}</div>
+          <h3 className="card__title">{title.toUpperCase()}</h3>
+          <div className="card__date">{date}</div>
+        </div>
       </div>
     </>
   );
